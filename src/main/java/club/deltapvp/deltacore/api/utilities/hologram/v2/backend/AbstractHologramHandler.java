9f27e4ba3dea8c581634d100834113ae6874dfd4
Negative <60104846+NegativeKB@users.noreply.github.com>
2021-10-04 16:18:52 -0230

    Update params

--- src/main/java/club/deltapvp/deltacore/api/utilities/hologram/v2/backend/AbstractHologramHandler.java
+++ src/main/java/club/deltapvp/deltacore/api/utilities/hologram/v2/backend/AbstractHologramHandler.java
@@ -3,16 +3,17 @@ package club.deltapvp.deltacore.api.utilities.hologram.v2.backend;
 import club.deltapvp.deltacore.api.DeltaPlugin;
 import club.deltapvp.deltacore.api.utilities.hologram.v2.Hologram;
 import org.bukkit.Location;
+import org.bukkit.entity.Player;
 
 import java.util.List;
 
 public abstract class AbstractHologramHandler {
 
-    public abstract void create(Hologram hologram);
+    public abstract void create(Player player, Hologram hologram, boolean all);
 
-    public abstract void remove(Hologram hologram);
+    public abstract void remove(Player player, Hologram hologram, boolean all);
 
-    public abstract void update(Hologram hologram);
+    public abstract void update(Player player, Hologram hologram, boolean all);
 
 
 }
