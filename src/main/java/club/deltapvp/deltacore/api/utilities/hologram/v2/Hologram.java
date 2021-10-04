2aac305827ad8f0c639fce406f4116912c89f863
Negative <60104846+NegativeKB@users.noreply.github.com>
2021-10-04 15:57:01 -0230

    Added hologram interact consumer to Hologram abstract class

--- src/main/java/club/deltapvp/deltacore/api/utilities/hologram/v2/Hologram.java
+++ src/main/java/club/deltapvp/deltacore/api/utilities/hologram/v2/Hologram.java
@@ -3,8 +3,11 @@ package club.deltapvp.deltacore.api.utilities.hologram.v2;
 import org.bukkit.Location;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 public abstract class Hologram {
+
+    private Consumer<HologramInteractEvent> hologramInteractEventConsumer;
 
     public abstract String handle();
 
@@ -12,4 +15,14 @@ public abstract class Hologram {
 
     public abstract Location location();
 
+    public void setHologramInteractEvent(Consumer<HologramInteractEvent> function) {
+        this.hologramInteractEventConsumer = function;
+    }
+
+    public void onHologramInteract(HologramInteractEvent event) {
+        if (hologramInteractEventConsumer == null)
+            return;
+
+        hologramInteractEventConsumer.accept(event);
+    }
 }
