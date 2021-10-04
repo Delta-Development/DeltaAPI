75f0f0482e50a1279bbb2858459b3c1bcf2b3e88
Negative <60104846+NegativeKB@users.noreply.github.com>
2021-10-04 15:19:16 -0230

    Update HologramManager

--- src/main/java/club/deltapvp/deltacore/api/utilities/hologram/v2/backend/HologramManager.java
+++ src/main/java/club/deltapvp/deltacore/api/utilities/hologram/v2/backend/HologramManager.java
@@ -2,10 +2,15 @@ package club.deltapvp.deltacore.api.utilities.hologram.v2.backend;
 
 import club.deltapvp.deltacore.api.DeltaPlugin;
 import club.deltapvp.deltacore.api.utilities.hologram.v2.Hologram;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 public abstract class HologramManager {
+
+    @Getter @Setter
+    private static HologramManager instance;
 
     public abstract void registerHologram(DeltaPlugin plugin, Hologram hologram);
 
